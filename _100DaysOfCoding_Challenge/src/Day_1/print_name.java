b100d99bf7d305b622c676024033d08cc2e6ab1f
shaikhanis <anisshaikh19059@gmail.com>
2023-01-05 12:24:01 +0530

    rite a program to print your name

---
+++ _100DaysOfCoding_Challenge/src/Day_1/print_name.java
@@ -0,0 +1,9 @@
+package Day_1;
+
+//rite a program to print your name
+
+public class print_name {
+    public static void main(String[] args) {
+        System.out.println("Shaikh Anis");
+    }
+}
